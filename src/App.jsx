3304d04579701951c35aa44b0a6fa5faa11eb3ce
A K M Shafee Ullah <187232344+shafee-ullah@users.noreply.github.com>
2025-08-05 10:05:11 +0600

    header and search function

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-
+import { useState } from 'react'
 import './App.css'
+import Search from './components/Search'
 
 function App() {
- 
+ const [searchTerm, setSearchTerm] = useState('')
 
   return (
     <>
-    <div>
-      <h1>Hello World</h1>
+    <div className='pattern'>
+     <div className='wrapper'>
+     <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+
+         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+     </div>
     </div>
     </>
   )
